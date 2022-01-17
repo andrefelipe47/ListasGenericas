72cc291e446f4ba4c13f25d55d80a3acc8c2c2a4
André Martins <andre.felipe47@hotmail.com>
2022-01-17 11:54:28 -0300

    Upload Inicial

---
+++ ListasGenericas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ListasGenericas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomesAnimais = new List<string>();
+
+            int quantidadeMaximaPerguntas = 5;
+
+            for (int i = 0; i < quantidadeMaximaPerguntas; i++)
+            {
+                Console.WriteLine("Qual o nome do animal?");
+                string nomeAnimal = Console.ReadLine();
+
+                nomesAnimais.Add(nomeAnimal);
+            }
+            nomesAnimais.Add("asdasd");
+            foreach (var nomeAnimal in nomesAnimais)
+            {
+                Console.WriteLine(nomeAnimal);
+            }
+        }
+    }
+}
